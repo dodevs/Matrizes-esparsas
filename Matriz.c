a9b189116c62b5a978b8e0afee4f20e67b84c0f3
dodevs <douglass.sousa@outlook.com.br>
2018-10-27 15:52:31 -0300

    Estrutua da matriz e implementacao parcial de insertInto()

--- Matriz.c
+++ Matriz.c
@@ -31,6 +31,8 @@ int matriz_getN(Matriz *matriz) {
 
 /* end of Getters */
 
+/* Setters */
+
 void matriz_setLinhas(Matriz *matriz, Cell **linhas) {
   matriz->linhas = linhas;
 }
@@ -51,59 +53,43 @@ void matriz_setN(Matriz *matriz, int n) {
 
 Matriz *matriz_newMatriz(int m, int n) {
   Matriz *matriz = (Matriz *)malloc(sizeof(Matriz));
-  matriz->colunas = matriz->linhas = (Cell**) malloc(sizeof(Cell*) * n);
-  matriz->m = n;
+  matriz->linhas = (Cell**) malloc(sizeof(Cell*) * m);
+  matriz->colunas = (Cell**) malloc(sizeof(Cell*) * n);
+  matriz->m = m;
   matriz->n = n;
 
   return matriz;
 }
 
 void matriz_insertInto(Matriz *matriz,int i, int j, int k) {
- Cell *ncell = cell_newCell(i, j, k); // Cria uma nova celula
  Cell **linhas = matriz_getLinhas(matriz); // Retorna o array de linhas da matriz
  Cell **colunas = matriz_getColunas(matriz); // Retorna o array de colunas da matriz
 
- if(linhas[i] == NULL){ // Se a linha da posicao for nulas
-   matriz->linhas[i-1] = ncell; // A linha daquela posicao passa a ter somente o elemento
- }else { // Se nao for nula
-   Cell *cell = *linhas; // Eu pego a cabeca da linha
-   while(cell != NULL) {
-     Cell *nextCell = cell_getNextOfRow(cell);
-     if(nextCell != NULL) {
-       if(cell_getJ(nextCell) > j) {
-         cell_setNextOfRow(cell, ncell);
-         cell_setNextOfRow(ncell, nextCell);
+ if(i <= matriz_getM(matriz) && j <= matriz_getN(matriz)) {
+   Cell *ncell = cell_newCell(i, j, k); // Cria uma nova celula
+
+   if(*(linhas+i) == NULL) { // Se a cabeça da linha for nula
+     *(linhas+i) = ncell; // Então a nova céula será a cabeça da mesma
+   }else{
+     if(cell_getJ(*(linhas+i)) == j) { // Se a cabeça da linha estiver na mesma coluna que quero inserir
+       Cell *auxL = cell_getNextOfRow(*(linhas+i)); // Salvo o ponteiro para a próxima da linha
+       Cell *auxC = cell_getNextOfCol(*(linhas+i)); // E o ponteiro para a próxima da colunas
+       cell_setNextOfRow(ncell,auxL); // Defino a próxima celula da linha na nova celula
+       cell_setNextOfCol(ncell,auxC); // E o mesmo com a próxima colunas
+       *(linhas+i) = ncell; // Agora a cabeça da linha aponta para a nova céula
+     } else { // Se nao for a primeira da lista, então tenho que percorrer para encontrar
+       Cell *ccell = *(linhas+i); // Armazeno a cabeça da linha
+       // Equanto o proximo da linha nao for nulo e menor que a posicao que quero inserir
+       while (cell_getNextOfRow(ccell) != NULL && cell_getJ(cell_getNextOfRow(ccell)) < j) {
+         ccell = cell_getNextOfRow(ccell); // `ccell` recebe a proxima celula da linha
        }
-       if(cell_getJ(nextCell) == j) {
-         cell_setNextOfRow(ncell, cell_getNextOfRow(nextCell));
-         cell_setNextOfRow(cell, ncell);
-         cell_killCell(nextCell);
-       }
+       // Atribui como próximo elemento de `ncell` o proximo da celula auxiliar `ccell`
+       cell_setNextOfRow(ncell, cell_getNextOfRow(ccell));
+       // Atribui como próximo elemento de `ccell` a nova celula criada
+       cell_setNextOfRow(ccell, ncell);
      }
-
-     cell = cell_getNextOfRow(cell);
    }
- }
 
- if(colunas[j] == NULL){ // Se a linha da posicao for nulas
-   matriz->colunas[j-1] = ncell; // A linha daquela posicao passa a ter somente o elemento
- }else { // Se nao for nula
-   Cell *cell = *colunas; // Eu pego a cabeca da linha
-   while(cell != NULL) {
-     Cell *nextCell = cell_getNextOfCol(cell);
-     if(nextCell != NULL) {
-       if(cell_getJ(nextCell) > i) {
-         cell_setNextOfCol(cell, ncell);
-         cell_setNextOfCol(ncell, nextCell);
-       }
-       if(cell_getJ(nextCell) == i) {
-         cell_setNextOfCol(ncell, cell_getNextOfCol(nextCell));
-         cell_setNextOfCol(cell, ncell);
-         cell_killCell(nextCell);
-       }
-     }
 
-     cell = cell_getNextOfCol(cell);
-   }
  }
 }
