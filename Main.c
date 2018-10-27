57e1fa74143c632140efc9daec7c1b5fc720c6c8
dodevs <douglass.sousa@outlook.com.br>
2018-10-27 15:52:44 -0300

    Modificados testes

--- Main.c
+++ Main.c
@@ -4,18 +4,18 @@
 
 int main(int argc, char *argv[]) {
 
-  int TMATRIZ = 4;
+  int TMATRIZ = 2;
 
   Matriz *mat = matriz_newMatriz(TMATRIZ,TMATRIZ);
 
-  matriz_insertInto(mat, 1, 1, 50);
-  matriz_insertInto(mat, 1, 2, 20);
+  matriz_insertInto(mat, 0, 0, 5);
+  matriz_insertInto(mat, 0, 1, 6);
 
   Cell **linhas = matriz_getLinhas(mat);
   Cell **colunas = matriz_getColunas(mat);
 
-  printf("Valor da linha 1: %d\n", cell_getK(linhas[0]));
-  printf("Valor da coluna 1: %d\n", cell_getK(colunas[0]));
+  printf("Valor da linha 0 coluna 0: %f\n", cell_getK(linhas[0]));
+  printf("Valor da linha 0 coluna 1: %f\n", cell_getK(cell_getNextOfRow(linhas[0])));
   // for(int i = 0; i < TMATRIZ; i++) {
   //   if(*(colunas+i) != NULL) printf("%d ", cell_getK(*(colunas+i)));
   //   else printf("%d ", 0);
